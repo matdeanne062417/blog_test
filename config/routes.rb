194dfc2296dc45bcbe021411bf226ce239e95048
matdeanne062417 <matthew.balagtas007@gmail.com>
2020-11-23 15:13:05 +0800

    added controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get '/categories' => 'categories#index'
+get '/categories/new' => 'categories#new', as: 'new_category'
+get '/categories/show' => 'categories#show', as: 'show_category'
+get '/categories/edit' => 'categories#edit', as: 'edit_category'
+
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,4 +4,25 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get new" do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get categories_url
+    assert_response :success
+  end
+  
+  test "should get show" do
+    get show_category_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_url
+    assert_response :success
+  end
+
 end
