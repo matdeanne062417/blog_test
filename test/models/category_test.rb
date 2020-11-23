596aff8153dbde09f0f6c57f51e47cfed1cecfbd
matdeanne062417 <matthew.balagtas007@gmail.com>
2020-11-23 13:48:30 +0800

    added model test for validation

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,17 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "name is required" do
+    category = Category.new
+    assert_not category.save
+  end
+
+ 
+  test "name should be unique" do
+      category = Category.create(name: "Example")
+      duplicate_category = category.dup 
+      assert_not duplicate_category.valid?
+  end
+
 end
